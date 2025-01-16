44ae96068b478800799b9edce529bed1ac1b0966
Rovall03 <rosaovalle@my.whccd.edu>
2025-01-15 19:59:29 -0800

    Add project files.

---
+++ Comp003.lectureactivity3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Author:Rosa Ovalle 
+ * Course:COMP003.LectureActivity1
+ * Faculty: Jonathan Cruz
+ * Purpose: This application demonstrates the basic structure of a .NET console application.
+ */
+
+//This namespace contains all the code for the application 
+namespace COMP003.LectureActivity1
+{
+    //This is the main class of the application
+    internal class Program
+    {
+        //The entry point of the aplication
+        static void Main(string[] args)
+        {
+            //displays a simple message 
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
